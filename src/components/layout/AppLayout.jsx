2351ea732ab4d56f3f7f30362f9af7611de0f499
Elgün Gasimov <bygsmv@gmail.com>
2026-04-18 23:29:08 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -18,15 +18,7 @@ export default function AppLayout() {
   const location = useLocation();
 
   useEffect(() => {
-    base44.auth.me().then((u) => {
-      setUser(u);
-      // If cashier, re-fetch to ensure custom fields (club_owner_id) are loaded
-      if (u?.role === 'user') {
-        base44.entities.User.filter({ id: u.id }).then((users) => {
-          if (users?.[0]) setUser({ ...u, ...users[0] });
-        });
-      }
-    });
+    base44.auth.me().then(setUser);
   }, []);
 
   // Redirect owner to /owner panel
